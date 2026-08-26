3b47505f655fcbeab2a54aa04572d113212f117c
Shay Davidovitch <ayeka@gmail.com>
2026-08-26 23:52:30 +0300

    Remove IDE files and secure database configuration

--- sqlserver_connection.py
+++ sqlserver_connection.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine
 import urllib
+import os
+
+server = os.getenv("DB_SERVER", "localhost")
+database = os.getenv("DB_NAME", "FireArc")
 
 connection = urllib.parse.quote_plus(
-    "Data Source Name=DESKTOP-7VT8EU7;"
-    "Driver={SQL Server};"
-    "Server=DESKTOP-7VT8EU7;"
-    "Database=FireArc;"
-    "Trusted_connection=yes"
+    f"Driver={{ODBC Driver 17 for SQL Server}};"
+    f"Server={server};"
+    f"Database={database};"
+    "Trusted_Connection=yes;"
 )
 
-con_eng = create_engine("mssql+pyodbc:///?odbc_connect={}".format(connection))
+con_eng = create_engine(
+    f"mssql+pyodbc:///?odbc_connect={connection}"
+)
